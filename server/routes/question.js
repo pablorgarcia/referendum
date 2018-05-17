0862b408d7c3fe6e061fa561db35f6faa0f9ccaf
Pablo García <heypablete@gmail.com>
2018-05-17 20:55:19 +0200

    fixing bugs

--- server/routes/question.js
+++ server/routes/question.js
@@ -14,14 +14,31 @@ router.get("/", (req, res, next) => {
     .catch(e => next(e));
 });
 
-const loc = ['city', 'country', 'continent'];
-  
 // por parametros
-router.get("/:location", (req, res, next) => {
-  Question.find({location: req.params.location})
+/*
+Cambias el get por un post
+Con el fin de mandarle en el body el objeto location del user
+Que contiene la ciudad, pais, y continente.
+Aqui dentro haces tres queries, cada con uno de esos campos.
+y pusheas a un array cada respuesta
+*/
+router.post("/location", (req, res, next) => {
+  let questions = [];
+  
+  Question.find({location: req.body.location.city})
     .sort({counter: -1})
-    .then(quests => res.json(quests))
+    .then(quests => {
+      questions.push(quests);
+      Question.find({location: req.body.location.country})
+        .then(quests => {
+          questions.push(quests);
+            Question.find({location: req.body.location.continen})
+            .then(quests => {
+              questions.push(quests);
+            })
+    })
     .catch(e => next(e));
+    })
 });
 
 // Create
@@ -34,8 +51,9 @@ router.post("/", (req, res, next) => {
 
 // Retrive DETAIL
 router.get("/:id", (req, res, next) => {
+  console.log('entra')
   Question.findById(req.params.id)
-    .then(quest => res.json(quest))
+    .then(quest => {res.json(quest);console.log(quest)})
     .catch(e => next(e));
 });
 
